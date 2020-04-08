192485bfe62717ec6d90001cd43e7aed83443d29
mesneym <mesney.mesney@gmail.com>
2020-04-08 11:25:23 -0400

    Added drone and environment

---
+++ parrot_ws/devel/share/gennodejs/ros/ardrone_as/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let ArdroneActionResult = require('./ArdroneActionResult.js');
+let ArdroneGoal = require('./ArdroneGoal.js');
+let ArdroneActionGoal = require('./ArdroneActionGoal.js');
+let ArdroneActionFeedback = require('./ArdroneActionFeedback.js');
+let ArdroneResult = require('./ArdroneResult.js');
+let ArdroneAction = require('./ArdroneAction.js');
+let ArdroneFeedback = require('./ArdroneFeedback.js');
+
+module.exports = {
+  ArdroneActionResult: ArdroneActionResult,
+  ArdroneGoal: ArdroneGoal,
+  ArdroneActionGoal: ArdroneActionGoal,
+  ArdroneActionFeedback: ArdroneActionFeedback,
+  ArdroneResult: ArdroneResult,
+  ArdroneAction: ArdroneAction,
+  ArdroneFeedback: ArdroneFeedback,
+};
