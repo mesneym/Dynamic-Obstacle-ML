0813c97b05896b06ca83ec47009bb54851c983a0
mesneym <mesney.mesney@gmail.com>
2020-04-14 08:53:38 -0400

    Working version

--- parrot_ws/devel/share/ardrone_as/cmake/ardrone_asConfig.cmake
+++ parrot_ws/devel/share/ardrone_as/cmake/ardrone_asConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ak/Dynamic-Obstacle-ML/parrot_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/ak/Dynamic-Obstacle-ML/parrot_ws/devel/lib;/home/ak/Differential-Drive-PP/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
